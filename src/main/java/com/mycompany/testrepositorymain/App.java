e8a99a5b8cbbc764a2d41f6da6377b4723d16441
lvotypko <lvotypko@redhat.com>
2014-07-17 17:03:11 +0200

    init

---
+++ src/main/java/com/mycompany/testrepositorymain/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.testrepositorymain;
+
+import com.mycompany.testrepositorysource.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Hello hello = new Hello("Hello");
+        hello.greetings();
+    }
+}
